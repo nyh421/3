11443ad5f7181cbdc343ab228dfe47e862c9fe7f
EC2 Default User <ec2-user@ip-172-31-95-190.ec2.internal>
2022-04-08 09:47:40 +0000

    [Add]プロフィール編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: "homes#top"
